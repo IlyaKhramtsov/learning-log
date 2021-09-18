84ebbb8c0e10f13a24876c09a1202779adff9867
Ilya <ilya.kramt@gmail.com>
2021-09-19 00:43:59 +0300

    add new route to topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 
 from learning_logs import views
 
+
+app_name = 'learning_logs'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
