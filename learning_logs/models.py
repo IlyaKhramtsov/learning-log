f7c660e71d20738cdb33fbf49b77aeaeff647a32
Ilya <ilya.kramt@gmail.com>
2021-09-18 20:42:11 +0300

    update string representation to Entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -22,4 +22,6 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
+        if len(self.text) < 50:
+            return f"{self.text}"
         return f"{self.text[:50]}..."
